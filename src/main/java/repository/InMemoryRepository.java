1501f83337b914755884d22c6c774c690fa643fc
Bogdan Diaconu <melingus99@gmail.com>
2020-03-08 12:54:29 +0200

    Changed a bit the structure of the project

--- src/main/java/repository/InMemoryRepository.java
+++ src/main/java/repository/InMemoryRepository.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.*;
 
-public class StudentRepository{
+public class StudentRepository extends Repository<{
     private ArrayList<Student> studentRepo;
 
     public StudentRepository(){
